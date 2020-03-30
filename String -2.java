6737151f4055628729b40a8d59bae91697ae75ae
SagarShubh <41685423+SagarShubh@users.noreply.github.com>
2020-03-30 23:12:09 +0530

    Update String -2.java

--- String -2.java
+++ String -2.java
@@ -11,3 +11,30 @@ public int countHi(String str) {
 	}
 	return times;
 }
+
+
+
+//Given two strings, return true if either of the strings appears at the very end of the other string,
+//ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+//Note: str.toLowerCase() returns the lowercase version of a string.
+
+public boolean endOther(String a, String b) {
+
+  int aLen = a.length();
+	int bLen = b.length();
+	String end;
+	String temp;
+	a = a.toLowerCase();
+	b = b.toLowerCase();
+	if(aLen >= bLen)
+	{
+		end = a.substring(aLen - bLen);
+		temp = b;
+	}
+	else
+	{
+		end = b.substring(bLen - aLen);
+		temp = a;
+	}
+	return (end.equals(temp));
+}
