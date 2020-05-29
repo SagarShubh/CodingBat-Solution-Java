be6fafd3926236808fac043c546137db1aaad5f3
SagarShubh <41685423+SagarShubh@users.noreply.github.com>
2020-05-30 00:30:40 +0530

    Create sum67.java

---
+++ Array/2/sum67.java
@@ -0,0 +1,27 @@
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+
+
+public int sum67(int[] nums) {
+  int sum =0;
+  boolean insideRange = false;
+  
+  for(int i =0; i< nums.length; i++){
+    if(nums[i] == 6)
+        insideRange = true;
+        
+    if(!insideRange)
+        sum += nums[i];
+        
+ 
+    if(insideRange && nums[i]==7)
+    insideRange = false;
+  
+  }
+  return sum;
+  
+}
